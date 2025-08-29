a18b64770a10d6c801b4885e3f8cfd2af0878804
AbrarUI12 <135985867+AbrarUI12@users.noreply.github.com>
2025-08-29 21:11:16 +0600

    Charecter selection done

--- project/main.py
+++ project/main.py
@@ -11,8 +11,8 @@ from player_movement import *
 
 # Window and world settings
 WIN_W, WIN_H = 1024, 720
-ARENA_HALF = 25.0
-GRID_STEP = 1.0
+ARENA_HALF = 100
+GRID_STEP = 5.0
 CAMERA_DIST = 10.0
 PLAYER_Y = 0.75
 MOVE_SPEED = 4.0
@@ -24,18 +24,126 @@ camPitchDeg = 20.0
 camDist = CAMERA_DIST
 PI = math.pi
 
+# Game states
+MENU = 0
+PLAYING = 1
+game_state = MENU
+
+# Selected model
+selected_model = None
+
+
+
+def draw_menu():
+    # --- Draw background and initial text in 2D ---
+    glDisable(GL_DEPTH_TEST)
+    glDisable(GL_LIGHTING)
+
+    glMatrixMode(GL_PROJECTION)
+    glPushMatrix()
+    glLoadIdentity()
+    gluOrtho2D(0, WIN_W, 0, WIN_H)
+
+    glMatrixMode(GL_MODELVIEW)
+    glPushMatrix()
+    glLoadIdentity()
+
+    # Background
+    glColor3f(0.1, 0.1, 0.15)
+    glBegin(GL_QUADS)
+    glVertex2f(0, 0)
+    glVertex2f(WIN_W, 0)
+    glVertex2f(WIN_W, WIN_H)
+    glVertex2f(0, WIN_H)
+    glEnd()
+
+    # Main menu text
+    glColor3f(1.0, 1.0, 1.0)
+    def draw_text(x, y, text):
+        glRasterPos2f(x, y)
+        for c in text.encode("utf-8"):
+            glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, c)
+
+    draw_text(WIN_W*0.35, WIN_H*0.8, "Select Your Character:")
+    draw_text(WIN_W*0.35, WIN_H*0.2, "Press 1 for Abrar\n 2 for Sanjoy\n 3 for Ishrak")
+
+    glPopMatrix()
+    glMatrixMode(GL_PROJECTION)
+    glPopMatrix()
+    glMatrixMode(GL_MODELVIEW)
+
+    # --- Draw 3D character models ---
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_LIGHTING)
+
+    # Abrar
+    glPushMatrix()
+    glTranslatef(-2.0, -1.5, -8.0)
+    glRotatef(30, 0, 1, 0)
+    Abrar_model()
+    glPopMatrix()
+
+    # Sanjoy
+    glPushMatrix()
+    glTranslatef(0.0, -1.5, -8.0)
+    glRotatef(-30, 0, 1, 0)
+    Sanjoy_model()
+    glPopMatrix()
+    
+    #ishrak
+    glPushMatrix()
+    glTranslatef(2.0,-1.5, -8.0)
+    glRotatef(-30, 0, 1, 0)
+    Ishrak_model()
+    glPopMatrix()
+
+    # --- Draw labels for models in 2D again ---
+    glDisable(GL_DEPTH_TEST)
+    glDisable(GL_LIGHTING)
+
+    glMatrixMode(GL_PROJECTION)
+    glPushMatrix()
+    glLoadIdentity()
+    gluOrtho2D(0, WIN_W, 0, WIN_H)
+    glMatrixMode(GL_MODELVIEW)
+    glPushMatrix()
+    glLoadIdentity()
+
+    # Draw model names
+    glColor3f(1.0, 1.0, 1.0)
+    draw_text(WIN_W*0.35, WIN_H*0.75, "Abrar")
+    draw_text(WIN_W*0.65, WIN_H*0.75, "Ishrak")
+    draw_text(WIN_W*0.50, WIN_H*0.75, "Sanjoy")
+    
+    glPopMatrix()
+    glMatrixMode(GL_PROJECTION)
+    glPopMatrix()
+    glMatrixMode(GL_MODELVIEW)
+
+    glEnable(GL_DEPTH_TEST)
+    glEnable(GL_LIGHTING)
+
 
 
 # Display
 def on_display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    setupCamera()
-    drawGrid(ARENA_HALF, GRID_STEP)
-    glPushMatrix()
-    glTranslatef(player.pos[0], player.pos[1], player.pos[2])
-    glRotatef(player.angDeg, 0, 1, 0)
-    Sanjoy_model(player.lying)
-    glPopMatrix()
+    if game_state == MENU:
+       pass
+       draw_menu()
+    elif game_state == PLAYING:
+        setupCamera()
+        drawGrid(ARENA_HALF, GRID_STEP)
+        glPushMatrix()
+        glTranslatef(player.pos[0], player.pos[1], player.pos[2])
+        glRotatef(player.angDeg, 0, 1, 0)
+        if selected_model == "Abrar":
+            Abrar_model(player.lying)
+        elif selected_model == "Sanjoy":
+            Sanjoy_model(player.lying)
+        elif selected_model == "Ishrak":
+            Ishrak_model(player.lying)
+        glPopMatrix()
     glutSwapBuffers()
 
 def on_reshape(w, h):
@@ -61,10 +169,24 @@ def init_gl():
 # Keyboard movement
 last_time = time.time()
 def on_keyboard(key, x, y):
-    global last_time
+    global last_time,selected_model,game_state
     current_time = time.time()
     dt = current_time - last_time
     last_time = current_time
+    
+    if game_state == MENU:
+        if key == b'1':
+            selected_model = "Abrar"
+            game_state = PLAYING
+        elif key == b'2':
+            selected_model = "Sanjoy"
+            game_state = PLAYING
+        elif key == b'3':
+            selected_model = "Ishrak"
+            game_state = PLAYING
+        glutPostRedisplay()
+        return
+
 
     key = key.decode('utf-8')
     if key == 'w':  # Forward
--- project/model.py
+++ project/model.py
@@ -16,49 +16,54 @@ def Abrar_model(lying=False):
     if lying:
         glRotatef(-90.0, 1, 0, 0)
 
+    leg_length = 1.5
+    body_height = 1.5
+    body_y_offset = leg_length  # move body above legs
+
     # Body (main cube)
     glPushMatrix()
-    glTranslatef(0.0, 0.5, 0.0)
+    glTranslatef(0.0, body_y_offset, 0.0)
     glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(1.0, 1.0, 0.6)  
+    drawCuboid(1.0, body_height, 0.6)  
     glPopMatrix()
    
-    #Right arm
+    # Right arm
     glPushMatrix()
-    glTranslatef(.65, 0.7, 0.0)
+    glTranslatef(.65, body_y_offset + 0.2, 0.0)
     glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(0.3, 0.7, 0.3) 
+    drawCuboid(0.3, 0.9, 0.3) 
     glPopMatrix()  
 
     # Left arm
     glPushMatrix()
-    glTranslatef(-0.65, 0.7, 0.0)
+    glTranslatef(-0.65, body_y_offset + 0.2, 0.0)
     glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(0.3, 0.7, 0.3)
+    drawCuboid(0.3, 0.9, 0.3)
     glPopMatrix()  
 
-    #head
+    # Head
     glPushMatrix() 
-    glTranslatef(0.0, 1.3, 0.0)  
+    glTranslatef(0.0, body_y_offset + 1.1, 0.0)  
     glColor3f(0.9, 0.8, 0.7)
     drawSphere(0.35)   
     glPopMatrix()  
 
     # Left leg
     glPushMatrix()
-    glTranslatef(-0.3, -0.8, 0.0)
+    glTranslatef(-0.3, leg_length/2, 0.0)
     glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(0.3, 1.5, 0.3)
+    drawCuboid(0.3, leg_length, 0.3)
     glPopMatrix()
 
     # Right leg
     glPushMatrix()
-    glTranslatef(0.3, -0.8, 0.0)
+    glTranslatef(0.3, leg_length/2, 0.0)
     glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(0.3, 1.5, 0.3)
+    drawCuboid(0.3, leg_length, 0.3)
     glPopMatrix()
 
     glPopMatrix()
+
     
 def Sanjoy_model(lying=False):
     glPushMatrix()
@@ -66,47 +71,110 @@ def Sanjoy_model(lying=False):
     if lying:
         glRotatef(-90.0, 1, 0, 0)
 
+    leg_length = 1
+    body_height = 1
+    body_y_offset = leg_length # body sits on top of legs
+
+    # Dark blue color
+    body_color = (0.0, 0.0, 0.3)
+
     # Body (main cube)
     glPushMatrix()
-    glTranslatef(0.0, 0.3, 0.0)
-    glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(1.0, 1.0, 0.6)  
+    glTranslatef(0.0, body_y_offset+.2, 0.0)
+    glColor3f(*body_color)
+    drawCuboid(1.0, body_height, 0.6)  
     glPopMatrix()
    
-    #Right arm
+    # Right arm
     glPushMatrix()
-    glTranslatef(.65, 0.5, 0.0)
-    glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(0.3, 0.7, 0.3) 
+    glTranslatef(.65, body_y_offset + 0.2, 0.0)
+    glColor3f(*body_color)
+    drawCuboid(0.3, 0.9, 0.3) 
     glPopMatrix()  
 
     # Left arm
     glPushMatrix()
-    glTranslatef(-0.65, 0.5, 0.0)
-    glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(0.3, 0.7, 0.3)
+    glTranslatef(-0.65, body_y_offset + 0.2, 0.0)
+    glColor3f(*body_color)
+    drawCuboid(0.3, 0.9, 0.3)
     glPopMatrix()  
 
-    #head
+    # Head
     glPushMatrix() 
-    glTranslatef(0.0, 1.1, 0.0)  
-    glColor3f(0.9, 0.8, 0.7)
+    glTranslatef(0.0, body_y_offset + 1.1, 0.0)  
+    glColor3f(0.9, 0.8, 0.7)  # skin color
     drawSphere(0.35)   
     glPopMatrix()  
 
     # Left leg
     glPushMatrix()
-    glTranslatef(-0.3, -0.8, 0.0)
-    glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(0.3, 1.5, 0.3)
+    glTranslatef(-0.3, leg_length/2, 0.0)
+    glColor3f(*body_color)
+    drawCuboid(0.3, leg_length, 0.3)
     glPopMatrix()
 
     # Right leg
     glPushMatrix()
-    glTranslatef(0.3, -0.8, 0.0)
-    glColor3f(0.09, 0.09, 0.09)
-    drawCuboid(0.3, 1.5, 0.3)
+    glTranslatef(0.3, leg_length/2, 0.0)
+    glColor3f(*body_color)
+    drawCuboid(0.3, leg_length, 0.3)
     glPopMatrix()
 
     glPopMatrix()
+
+
+def Ishrak_model(lying=False):
+    glPushMatrix()
+
+    if lying:
+        glRotatef(-90.0, 1, 0, 0)
+
+    leg_length = 1.55
+    body_height = 1.6
+    body_y_offset = leg_length  # move body above legs
+
+    # Body (main cuboid)
+    glPushMatrix()
+    glTranslatef(0.0, body_y_offset, 0.0)
+    glColor3f(0.5, 0.0, 0.5)  # Purple
+    drawCuboid(1.2, body_height, 0.6)
+    glPopMatrix()
+
+    # Right arm
+    glPushMatrix()
+    glTranslatef(0.65, body_y_offset + 0.2, 0.0)
+    glColor3f(0.5, 0.0, 0.5)
+    drawCuboid(0.3, 0.9, 0.3)
+    glPopMatrix()
+
+    # Left arm
+    glPushMatrix()
+    glTranslatef(-0.65, body_y_offset + 0.2, 0.0)
+    glColor3f(0.5, 0.0, 0.5)
+    drawCuboid(0.3, 0.9, 0.3)
+    glPopMatrix()
+
+    # Head
+    glPushMatrix()
+    glTranslatef(0.0, body_y_offset + 1.1, 0.0)
+    glColor3f(0.9, 0.8, 0.7)  # Skin tone
+    drawSphere(0.35)
+    glPopMatrix()
+
+    # Left leg
+    glPushMatrix()
+    glTranslatef(-0.3, leg_length / 2, 0.0)
+    glColor3f(0.5, 0.0, 0.5)
+    drawCuboid(0.3, leg_length, 0.3)
+    glPopMatrix()
+
+    # Right leg
+    glPushMatrix()
+    glTranslatef(0.3, leg_length / 2, 0.0)
+    glColor3f(0.5, 0.0, 0.5)
+    drawCuboid(0.3, leg_length, 0.3)
+    glPopMatrix()
+
+    glPopMatrix()
+
     
